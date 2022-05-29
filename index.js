bff16a52ede8ed700d4a9378eb4016dd01764f11
Muhammad Fachri Maulana <mfachri4@gmail.com>
2022-05-29 15:56:51 +0700

    feat: Challenge StrukturData

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-// first user favorite color and favoriteRestaurant
-
-
 const firstUser = {
     name:"Gurobah",
     gender:"Male",
