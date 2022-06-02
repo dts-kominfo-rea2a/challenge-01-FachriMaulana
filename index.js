7c2e14b1543856d203ee1b6cb4b8e98243ee2277
Muhammad Fachri Maulana <mfachri4@gmail.com>
2022-06-02 11:19:32 +0700

    Challenge ke 1

--- index.js
+++ index.js
@@ -2,52 +2,48 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {
-    name:"Gurobah",
-    gender:"Male",
-    age:25,
-    email:"Gurobah@gmail.com",
-    favoriteColor:["Blue","White"],
-    isHavePet:"No",
-    education:{
-        dataEdu:[
-            { name: "SD Pagi Sore", city: "Lampung", graduate: "2009" },
-            { name: "SMP Pagi Sore", city: "Lampung", graduate: "2012" },
-            { name: "SMA Pagi Sore", city: "Lampung", graduate: "2015" },
-            { name: "Universitas Jakarta", city: "Jakarta", graduate: "2019" }
-        ]
-    },
-    favoriteRestaurant:["Martabak", "Sate"]
+  name: "Gurobah",
+  gender: "Male",
+  age: 25,
+  email: "Gurobah@gmail.com",
+  favoriteColor: ["Blue", "White"],
+  isHavePet: "No",
+  education: [
+    { name: "SD Pagi Sore", city: "Lampung", graduate: "2009" },
+    { name: "SMP Pagi Sore", city: "Lampung", graduate: "2012" },
+    { name: "SMA Pagi Sore", city: "Lampung", graduate: "2015" },
+    { name: "Universitas Jakarta", city: "Jakarta", graduate: "2019" },
+  ],
+  favoriteRestaurant: ["Martabak", "Sate"],
 };
 
 const secondUser = {
-    name:"Salsa",
-    gender:"Female",
-    age:24,
-    email:"Salsa@gmail.com",
-    favoriteColor:["Red","Yellow","Green"],
-    isHavePet:"Yes",
-    education:{
-        dataEdu:[
-            { name: "SD Pagi", city: "Bogor", graduate: "2010" },
-            { name: "SMP Siang", city: "Bogor", graduate: "2013" },
-            { name: "SMA Sore", city: "Bogor", graduate: "2016" },
-            { name: "Universitas Bogor", city: "Bogor", graduate: "2020" }
-        ]
-    },
-    favoriteRestaurant:["Soto", "Nasi Goreng", "Bakso"]
+  name: "Salsa",
+  gender: "Female",
+  age: 24,
+  email: "Salsa@gmail.com",
+  favoriteColor: ["Red", "Yellow", "Green"],
+  isHavePet: "Yes",
+  education: [
+    { name: "SD Pagi", city: "Bogor", graduate: "2010" },
+    { name: "SMP Siang", city: "Bogor", graduate: "2013" },
+    { name: "SMA Sore", city: "Bogor", graduate: "2016" },
+    { name: "Universitas Bogor", city: "Bogor", graduate: "2020" },
+  ],
+  favoriteRestaurant: ["Soto", "Nasi Goreng", "Bakso"],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser,secondUser];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
