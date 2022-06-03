80719c1c0ecdf2e9713140e2eb5cd48e823d7b7e
Muhammad Fachri Maulana <mfachri4@gmail.com>
2022-06-03 10:17:57 +0700

    feat: penambahan Set

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+const firstFavColor = ["Yellow", "Pink", "White", "Purple"];
+const secondFavColor = ["Blue", "Black", "Grey"];
+
+const firstFavResto = [
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Padang",
+  "Tteok",
+];
+const secondFavResto = ["Katsu", "Geprek"];
+
 const firstUser = {
   name: "Monica",
   gender: "Female",
   age: 17,
   email: "monica@dingdong.com",
-  favoriteColor: ["Yellow", "Pink", "White", "Purple"],
+  favoriteColor: [...new Set(firstFavColor)],
   isHavePet: "Yes",
   education: [
     { name: "SD 01", city: "Jakarta", graduate: "2016" },
     { name: "SMP 02", city: "Jakarta", graduate: "2019" },
     { name: "SMA 03", city: "Tangerang" },
   ],
-  favoriteRestaurant: [
-    "Bento",
-    "Sushi",
-    "Pancake",
-    "Eggy",
-    "Tempura",
-    "Padang",
-    "Tteok",
-  ],
+  favoriteRestaurant: [...new Set(firstFavResto)],
 };
 
 const secondUser = {
@@ -29,7 +35,7 @@ const secondUser = {
   gender: "Male",
   age: 23,
   email: "wendy@dingdong.com",
-  favoriteColor: ["Blue", "Black", "Grey"],
+  favoriteColor: [...new Set(secondFavColor)],
   isHavePet: "No",
   education: [
     { name: "SD 02", city: "Jakarta", graduate: "2010" },
@@ -37,7 +43,7 @@ const secondUser = {
     { name: "SMA 01", city: "Surabaya", graduate: "2016" },
     { name: "Universitas Maju", city: "Tangerang" },
   ],
-  favoriteRestaurant: ["Katsu", "Geprek"],
+  favoriteRestaurant: [...new Set(secondFavResto)],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
